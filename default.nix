d248e296001c9984f9e686b9e3d04336be51dda5
Ben Siraphob <bensiraphob@gmail.com>
2020-12-28 13:43:11 +0700

    Fix building on different system

--- default.nix
+++ default.nix
@@ -1,8 +1,9 @@
 { sources ? import ./nix/sources.nix { }
 , pkgs ? import sources.nixpkgs { }
+, system ? builtins.currentSystem
 }:
 let
   # Nixpkgs extended with bootstrappable related packages
-  siraben-pkgs = import ./pkgs { };
+  siraben-pkgs = import ./pkgs { inherit system; };
 in
 siraben-pkgs
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,6 @@
-{ sources ? import ../nix/sources.nix, pkgs ? import sources.nix }:
+{ sources ? import ../nix/sources.nix, pkgs ? import sources.nix, system }:
 
 import sources.nixpkgs {
+  inherit system;
   overlays = [ (import ./packages.nix) ];
 }
