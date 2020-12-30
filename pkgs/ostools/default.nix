002f7fe7ef156e39551a9f0005dc3d4d630043d3
Ben Siraphob <bensiraphob@gmail.com>
2020-12-30 20:05:12 +0700

    ostools: init at unstable-2004-09-02

---
+++ pkgs/ostools/default.nix
@@ -0,0 +1,36 @@
+{ stdenv, fetchzip }:
+
+stdenv.mkDerivation rec {
+  pname = "ostools";
+  version = "unstable-2004-09-02";
+
+  src = fetchzip {
+    url = "https://www.ticalc.org/pub/unix/ostools.zip";
+    sha256 = "1jrwm1x68zg0bawgxdgw0gfyjyj36f7l56xx98wm3djg0n8xlv96";
+  };
+
+  buildPhase = ''
+    $CC -O2 -o multihex multihex.c
+    $CC -O2 -o packxxu packxxu.c
+    $CC -O2 -o rompatch rompatch.c
+  '';
+
+  installPhase = ''
+    install -Dm755 multihex packxxu rompatch -t $out/bin
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Useful programs for creating apps and OSes for the TI 83+/84+/73";
+    longDescription = ''
+      Three programs that are useful for creating both Apps and OSes
+      for the TI 83+/84+/73: multihex, a program to add page headers
+      to your hex files; rompatch, a program to install TI-Hex files
+      into a binary ROM image; and packxxu, a program to generate 8XU
+      and 73U files.
+    '';
+    homepage = "https://www.ticalc.org/archives/files/fileinfo/350/35057.html";
+    license = licenses.gpl2Plus;
+    maintainers = with maintainers; [ siraben ];
+    platforms = platforms.all;
+  };
+}
