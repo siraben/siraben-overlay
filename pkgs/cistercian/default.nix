7354130dbb38690ff25caee11b616e87c3929f63
Ben Siraphob <bensiraphob@gmail.com>
2021-08-25 22:33:09 -0500

    cistercian: remove leading space from meta.description

--- pkgs/cistercian/default.nix
+++ pkgs/cistercian/default.nix
@@ -14,7 +14,7 @@ buildGoModule rec {
   vendorSha256 = "sha256-XL4MIP67jbR+EOI3fhZ62LMnPdLXI1bG0ptgznd3REk";
 
   meta = with lib; {
-    description = " Command line tool to generate Cistercian numerals";
+    description = "Command line tool to generate Cistercian numerals";
     homepage = "https://github.com/rhardih/cistercian";
     license = licenses.mit;
     maintainers = with maintainers; [ siraben ];
