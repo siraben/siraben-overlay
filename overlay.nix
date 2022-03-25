d823efe187b4957a17fc57baa69e7dcdbafa5e8c
Ben Siraphob <bensiraphob@gmail.com>
2022-03-24 22:02:29 -0500

    morsel: update (upstreamed)

--- overlay.nix
+++ overlay.nix
@@ -30,7 +30,6 @@ in
   mes = callPackage ./pkgs/mes { };
   mescc-tools-seed = callPackage ./pkgs/mescc-tools-seed { };
   micro-lisp = callPackage ./pkgs/micro-lisp { };
-  morsel = callPackage ./pkgs/morsel { };
   nix-hello = callPackage ./pkgs/nix-hello { };
   nix-hello-minimal = callPackage ./pkgs/nix-hello-minimal { };
   ostools = callPackage ./pkgs/ostools { };
