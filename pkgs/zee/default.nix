c952cd280352315efe3213d5367ef1e8fd999798
Ben Siraphob <bensiraphob@gmail.com>
2022-04-11 19:58:35 -0500

    zee: unstable-2022-04-01 -> unstable-2022-04-11

--- pkgs/zee/default.nix
+++ pkgs/zee/default.nix
@@ -2,23 +2,23 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "zee";
-  version = "unstable-2022-04-01";
+  version = "unstable-2022-04-11";
 
   src = fetchFromGitHub {
     owner = "mcobzarenco";
     repo = "zee";
-    rev = "c2c943f6dde80a6344d2b6ec4b0487b76efb3990";
-    sha256 = "sha256-jFTk1lDcdiU7W2CE8yiKDespayEZZiN4275wnaJX5A4=";
+    rev = "a8979be0819a7140648315c1e5fac052933f9b57";
+    sha256 = "sha256-qCslkvur8aHAveYsX9i7DGpqySbVr4uOHDh3G/VnCco=";
     fetchSubmodules = true;
   };
 
-  cargoSha256 = "sha256-59mbVo2IG1T2AIm48PzU8gbTAQaBav3hAxlLY0xdSCA=";
+  cargoSha256 = "sha256-R8fYeZhEKracVAUvPsw690ZngKzncCarq0arfD8ZPyM=";
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ openssl ] ++ lib.optionals stdenv.isDarwin [ Security AppKit ];
 
   meta = with lib; {
-    description =g"A modern text editor for the terminal";
+    description = "A modern text editor for the terminal";
     homepage = "https://github.com/mcobzarenco/zee";
     license = licenses.asl20;
     maintainers = with maintainers; [ siraben ];
